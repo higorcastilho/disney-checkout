4c2352ac4b20847f6e09e9c55593b1f4f1a53706
Higor Castilho <castilhohf@gmail.com>
2020-12-15 14:16:14 -0300

    fix: to deploy on netlify

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import DisneyLogo from '../../assets/images/disney_logo.svg'
 
 import './styles.css'
@@ -7,7 +9,7 @@ import './styles.css'
 const Header = () => {
 	return (
 		<header id="checkout-header">
-			<section><p>Entrar</p></section>
+			<section><p><Link to="/checkout-email">Entrar</Link></p></section>
 			<section><img src={ DisneyLogo } alt=""/></section>
 		</header>
 	)
