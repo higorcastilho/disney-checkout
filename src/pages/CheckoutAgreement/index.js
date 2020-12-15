b3a991453f6aebe6e0f2208e1ea05899bf12a4b9
Higor Castilho <castilhohf@gmail.com>
2020-12-15 13:55:47 -0300

    add website on netlify

--- src/pages/CheckoutAgreement/index.js
+++ src/pages/CheckoutAgreement/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import './styles.css'
 
 function CheckoutAgreement () {
 	return (
-		<div>
-			<main id="checkout-agreement"></main>
+		<div id="checkout-agreement-wrapper">
+			<main id="checkout-agreement-main">
+				<p>PASSO 2 DE 4</p>
+				<h3>Contrato de Assinatura</h3>
+			</main>
+			<article id="checkout-agreement-article">				
+			</article>
+			<section>
+				<div>
+					Ao clicar em "Concordar e Continuar", você confirma que leu nosso
+					Contrato de Assinatura na íntegra e concorda com ele.
+				</div>
+				<Link to="/checkout-agreement">CONCORDAR E CONTINUAR</Link>
+			</section>
+
 		</div>
 	)
 }
